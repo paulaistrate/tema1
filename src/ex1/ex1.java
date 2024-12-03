a7793377fb4dfaf7f3b716a567b0b9076094dbed
paulaistrate <paulaistrate2014@gmail.com>
2024-12-03 18:16:35 +0200

    primul commit

--- src/ex1/ex1.java
+++ src/ex1/ex1.java
@@ -1,7 +1,19 @@
 package ex1;
 
+import java.util.Scanner;
+
 public class ex1 {
     public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("lungime= ");
+        int lungime = scanner.nextInt();
+        System.out.println("latime= ");
+        int latime = scanner.nextInt();
 
+        int arie=latime*lungime;
+        int perimetru=2*latime+2*lungime;
+        System.out.println("perimetrul este : " +perimetru);
+        System.out.println("aria este : " +arie);
+        scanner.close();
     }
 }
